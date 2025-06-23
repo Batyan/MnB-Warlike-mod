4a0f1694b5abda27815194d5d1f8900e3c328bb5
Batyan <yann.batt@gmail.com>
2025-06-23 22:49:48 +0200

    Skill description
    * update skills/attributes description

--- Src/module_skills.py
+++ Src/module_skills.py
@@ -1,13 +1,9 @@
 from header_common import *
 from header_skills import *
 
-
-
-
-
 skills = [
   ("trade","Trade",sf_base_att_cha|sf_effects_party,10,"Every level of this skill reduces your trade penalty. (Party skill)"),
-  ("leadership","Leadership",sf_base_att_cha,10,"Every point reduces troop wages by 2%% and increase troops morale by 10%%. (Leader skill)"),
+  ("leadership","Leadership",sf_base_att_cha,10,"Every point reduces troop wages by 2%%. (Leader skill)"),
   ("prisoner_management", "Prisoner Management",sf_base_att_cha,10,"Every level of this skill increases maximum number of prisoners by +5 + 10%% of your party size and reduces the penalties of having prisoners in your party. (Leader skill)"),
   ("reserved_1","Reserved Skill 1",sf_base_att_cha|sf_inactive,10,"This is a reserved skill."),
   ("reserved_2","Reserved Skill 2",sf_base_att_cha|sf_inactive,10,"This is a reserved skill."),
@@ -21,10 +17,10 @@ skills = [
   ("inventory_management","Inventory Management",sf_base_att_int,10,"Increases inventory capacity by +6 per skill level. (Leader skill)"),
   ("spotting","Spotting",sf_base_att_int|sf_effects_party,10,"Party seeing range is increased by 10%% per skill level. (Party skill)"),
   ("pathfinding","Path-finding",sf_base_att_int|sf_effects_party,10,"Party map speed is increased by 3%% per skill level. (Party skill)"),
-  ("tactics","Tactics",sf_base_att_int|sf_effects_party,10,"Every two levels of this skill increases starting battle advantage by 1, also increases party morale. (Party skill)"),
+  ("tactics","Tactics",sf_base_att_int|sf_effects_party,10,"Every two levels of this skill increases starting battle advantage by 1 and party morale by 10%%. (Party skill)"),
   ("tracking","Tracking",sf_base_att_int|sf_effects_party,10,"Tracks become more informative. (Party skill)"),
   ("trainer","Trainer",sf_base_att_int|sf_inactive,10,"Every day, each hero with this skill adds some experience to every other member of the party whose level is lower than his/hers. Experience gained goes as: {0,4,10,16,23,30,38,46,55,65,80}. (Personal skill)"),
-  ("trainer_2","Trainer",sf_base_att_int,10,"This skill helps you train and recruit new troops more quickly for a lower price. Trains hero companions' weapon proficiencies. (Personal skill)"),
+  ("trainer_2","Trainer",sf_base_att_int,10,"This skill helps you train and recruit new troops faster and for a lower price. Trains hero companions' weapon proficiencies. (Personal skill)"),
   ("reserved_6","Reserved Skill 6",sf_base_att_int|sf_inactive,10,"This is a reserved skill."),
   ("reserved_7","Reserved Skill 7",sf_base_att_int|sf_inactive,10,"This is a reserved skill."),
   ("reserved_8","Reserved Skill 8",sf_base_att_int|sf_inactive,10,"This is a reserved skill."),
